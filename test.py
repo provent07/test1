10d8f9b646de411f033513ff4466c0f0cf1f134e
Adriel Soengadi <iamsoengadi@gmail.com>
2018-09-18 15:58:25 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,39 @@
+# print("Hello World")
+# print('H','E','L','L','O')
+# x=2
+# y=7
+# print(x**y)
+# count=0
+# while (count <7):
+#     count=count+1
+#     print("*"*count)
+# count=0
+# num=7
+# while (count<num):
+#     count=count+1
+#     print(" "*(num-count)+ "*"*count)
+# count=0
+# num=7
+# while (count<num):
+#     count=count+1
+#     print((" "*(num-count)+ "*"*count +"*"*(count-1)+" "*(num-count))*7)
+#     print(("*" * (num - count) + " " * count + " " * (count - 1) + "*" * (num - count)) * 7)
+# count=0
+# num=7
+# while (count<num):
+#     count=count+1
+#     print(("*"*(num-count)+ " "*count +" "*(count-1)+"*"*(num-count))*7)
+# count=0
+# num=7
+# while (count<num):
+#     count=count+1
+#     print((" " * (num - count) + "*" * count + "*" * (count - 1) + " " * (num - count)) * 7)
+C=0
+print("C   " + "F")
+while (C<21):
+    F = (9 * C) / 5 + 32
+    print (C, F)
+    C=C+1
+
+
+
